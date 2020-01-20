9d1027a6ce32f0ea17251cd3b7a57cc2b8018d5d
wenhao <840346763@qq.com>
2020-01-20 17:19:01 +0800

    a

--- day1/demo.py
+++ day1/demo.py
@@ -10,3 +10,52 @@
 # a += 3
 # print(a)
 
+# 判断
+# if 1==2:
+# 	print('true')
+# else :
+# 	print('false')
+
+# 循环
+# list = [1, 1, 2, 3, 4]
+#
+# for i in list:
+# 	print(i)
+# range(1, 12)
+
+# 数组
+# member = ['小米', '小于', '小易']
+# number = [1, 2, 3, 4, 5]
+# print(member[1])
+# print(member[0:2])
+# print(member[0:])
+# print(member[:2])
+# print(member[:])
+# member.remove('小米')
+# del member
+
+# 元组 不可以修改里面的元素 ()创建空元组
+# list = (1, 2, 3, 4, 3, 5, 6)
+# print(list)
+
+# 字符串操作 可以想数组一样操作
+# str = 'hello world'
+# str[1]
+
+# 创建函数 关键字 def
+# def myDef():
+	# print('hello world')
+
+# 函数调用
+# myDef(hello, world)
+
+# 关键字参数 用于识别传参
+# meDef(hello= '123', world= '456')
+
+# 在函数内定义的都是局部变量 外部定义的全局
+# def myDef():
+# 	global 可以改变局部变量 变成全局变量
+	# name = '123' # 局部变量
+	# return  name
+
+# 函数支持 内嵌函数, 也是函数中再定义函数 调用
